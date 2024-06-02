eadec1348ea04dd684ddf6701a74377289cfb5be
Kishanmaisuria <99621357+Kishanmaisuria@users.noreply.github.com>
2024-06-02 18:42:10 +1200

    Feedback form + updates

--- obj/Debug/net6.0/Capstone_23_Proteine.AssemblyInfo.cs
+++ obj/Debug/net6.0/Capstone_23_Proteine.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Capstone_23_Proteine")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2faf1055d2cb5ad655fc8456a9d3bc3131e4ff52")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66ce2a9bc4058293059e996eef2ed604ee2fa3b0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Capstone_23_Proteine")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Capstone_23_Proteine")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
