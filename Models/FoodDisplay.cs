f8629e5b9cff32530042270b4dc663624ab32c3c
ChristopherDronjak <yoschmoo@gmail.com>
2024-06-03 12:16:58 +1200

    pt 2

---
+++ Models/FoodDisplay.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Capstone_23_Proteine.Models
+{    
+//    public class FoodDisplay
+//    {
+//        public Guid Id { get; set; }
+//        public int Protein { get; set; }
+//        public int Calories { get; set; }
+//        public int Fat { get; set; }
+//        public string MealName { get; set; }
+//    }
+}
